b599e0d5e9cfdaecda12b218a3c5e0d6e7949148
Zack <zacharyrock@hotmail.com>
2015-04-08 19:21:51 -0700

    Initial commit. Unity Ads imported. Unity ads usable scripts made.

---
+++ Assets/AdManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour {
+	void Awake() {
+		if (Advertisement.isSupported) {
+			Advertisement.allowPrecache = true;
+			Advertisement.Initialize ("30604", true);
+		} else {
+			Debug.Log("Platform not supported");
+		}
+	}
+
+	void OnGUI() {
+		if(GUI.Button(new Rect(10, 10, 150, 50), Advertisement.isReady() ? "Show Ad" : "Waiting...")) {
+			// Show with default zone, pause engine and print result to debug log
+			Advertisement.Show(null, new ShowOptions {
+				pause = true,
+				resultCallback = result => {
+					Debug.Log(result.ToString());
+				}
+			});
+		}
+	}
+}
